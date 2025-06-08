a01fccbc6a8c365234219d69ad9d9c9adaa08455
mahesh bhatiya <maheshbhatiya304@gmail.com>
2025-06-08 20:24:04 +0530

    feat(domain): add shell user and public_html dir creation for domain

--- config/apache/apache.go
+++ config/apache/apache.go
@@ -6,23 +6,23 @@ import "fmt"
 type ApacheConfig struct{}
 
 // Generate creates an Apache virtual host configuration
-func (a *ApacheConfig) Generate(domain, port string) (string, error) {
+func (a *ApacheConfig) Generate(domain, port, username string) (string, error) {
     vhostTemplate := `<VirtualHost *:%s>
     ServerName %s
     ServerAlias www.%s
-    DocumentRoot /var/www/%s
+    DocumentRoot /home/%s/public_html
     ErrorLog ${APACHE_LOG_DIR}/%s-error.log
     CustomLog ${APACHE_LOG_DIR}/%s-access.log combined
-    <Directory /var/www/%s>
+    <Directory /home/%s/public_html>
         Options Indexes FollowSymLinks
         AllowOverride All
         Require all granted
     </Directory>
 </VirtualHost>`
 
-    config := fmt.Sprintf(vhostTemplate, port, domain, domain, domain, domain, domain, domain)
-    return config, nil
+    return fmt.Sprintf(vhostTemplate, port, domain, domain, username, domain, domain, username), nil
 }
+
 
 // GetFileExtension returns the file extension for Apache config files
 func (a *ApacheConfig) GetFileExtension() string {
